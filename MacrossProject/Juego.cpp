6a81bd791e0eae59a80435c31e9d00ab3a82d719
javiereberra <javier_berra@live.com>
2023-12-18 17:52:42 -0300

    Agregar archivos de proyecto.

---
+++ MacrossProject/Juego.cpp
@@ -0,0 +1,108 @@
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include "Juego.h"
+
+using namespace sf;
+using namespace std;
+
+Juego::Juego(int ancho, int alto, std::string titulo) {
+
+	//Ventana
+	ventana1 = new RenderWindow(VideoMode(ancho, alto), titulo);
+
+	//Textura y Sprite de fondo
+	textura1 = new Texture;
+	fondo = new Sprite;
+	textura1->loadFromFile("assets/fondo1.jpg");
+	fondo->setTexture(*textura1);
+	fondo->setScale(2.0f,2.0f);
+	
+	fondoSpeed = 0.02f;
+
+	//para que siempre inicie el menú
+	start = false;
+
+}
+
+//metodo para iniciar un menu simple antes de iniciar el juego
+void Juego::ejecutar() {
+
+	while (ventana1->isOpen()) {
+		Event evento;
+		while (ventana1->pollEvent(evento)) {
+			if (evento.type == Event::Closed)
+				ventana1->close();
+			//Presionar "s" para iniciar el gameloop
+			if (evento.type == Event::KeyPressed) {
+				if (evento.key.code == Keyboard::Key::S && !start) {
+
+					start = true;
+
+
+				}
+			}
+		}
+
+		ventana1->clear(Color::Black);
+
+		if (start) {
+			//iniciar el loop si start es true
+			gameLoop();
+
+		}
+		else {
+			//Menu de fondo si start es false
+			ventana1->draw(*fondo);
+			
+			
+		}
+
+		ventana1->display();
+	}
+}
+
+void Juego::gameLoop() {
+
+
+	while (ventana1->isOpen() && start) {
+
+		procesar_eventos();
+		actualizar();
+
+		dibujar();
+
+	}
+}
+
+void Juego::procesar_eventos() {
+	Event evento1;
+	while (ventana1->pollEvent(evento1)) {
+		switch (evento1.type) {
+		case Event::Closed:
+			ventana1->close();
+			break;
+		
+
+		}
+	}
+}
+
+void Juego::actualizar() {
+	fondo->move(-fondoSpeed, 0);
+
+	if (fondo->getPosition().x <= (-665.0f)) {
+	fondo->setPosition(0, 0); // Reinicia la posición del fondo
+	}
+}
+
+void Juego::dibujar() {
+
+	ventana1->clear();
+
+	ventana1->draw(*fondo);
+	
+	ventana1->display();
+
+
+}
---
+++ MacrossProject/Juego.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+
+using namespace std;
+using namespace sf;
+
+class Juego {
+
+private:
+
+	//variables ventana
+	RenderWindow* ventana1;
+
+	//variables para el fondo
+	Texture* textura1;
+	Sprite* fondo;
+	bool start;
+	float fondoSpeed;
+
+public:
+	//constructor de juego
+	Juego(int ancho, int alto, std::string titulo);
+
+	//método para iniciar el juego desde un menú
+	void ejecutar();
+	//métodos de bucle, dibujo, actualizar y procesar eventos
+	void gameLoop();
+	void dibujar();
+	void actualizar();
+	void procesar_eventos();
+	// método para gestionar el disparo del mouse y colisiones
+
+	//destructor de juego
+	~Juego();
+};
